38ba56fb28aab25747e77f7c22d44496fa66d34d
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-04 18:23:41 +0200

    add schema and resolvers

---
+++ src/schema/index.ts
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+  type Concert {
+    id: ID!
+    venue: String!
+    venueLink: String!
+    city: String!
+    country: String!
+    ticketsLink: String!
+    concertDate: String!
+  }
+
+  type Query {
+    getConcerts: [Concert]
+    getConcertById(id: ID!): Concert
+  }
+
+  type Mutation {
+    addConcert(
+     venue: String!
+    venueLink: String!
+    city: String!
+    country: String!
+    ticketsLink: String!
+    concertDate: String!
+    ): Concert
+  }
+`;
