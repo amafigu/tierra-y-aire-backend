eea8dbedde6298d44bedd6fa428279c9c3d3aa59
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-09 17:10:29 +0200

    entrypoint

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,39 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApolloServer } from 'apollo-server-express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express from 'express';
-import { resolvers } from './resolvers';
-import { typeDefs } from './schema';
+import express, { type Application } from 'express';
+import { resolvers } from './graphql/resolvers';
+import { typeDefs } from './graphql/typeDefs';
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+const envFile =
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : process.env.NODE_ENV === 'test'
+      ? '.env.test'
+      : '.env.development';
 dotenv.config({ path: envFile });
-
-const app = express();
+const app: Application = express();
 app.use(cors());
 
 const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 async function start() {
   await apolloServer.start();
   apolloServer.applyMiddleware({ app: app as any });
-  const PORT = process.env.PORT || 4000;
+
+  const port = process.env.PORT;
+  const PORT = port ?? '4000';
   app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+    console.log(
+      `Server ready http://localhost:${PORT}${apolloServer.graphqlPath}`,
+    );
   });
 }
 
-start();
+start().catch((error) => {
+  console.error('Error starting the server:', error);
+});
